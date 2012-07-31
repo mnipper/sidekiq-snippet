be62309669a031c764cfd6b73d7d7fc4fab79ff0
Michael Nipper <mjn4406@gmail.com>
2012-07-30 21:54:58 -0400

    Add language validation.

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,6 @@
 class Snippet < ActiveRecord::Base
   attr_accessible :language, :plain_code, :highlighted_code
+
+  validates :language, :inclusion => { :in => %w[ruby python javascript css] }
+
 end
